df70ac6440d8f661df0b0b6d2dbfbeb085585fb6
Lukas Landwich <landwich.lukas@gmail.com>
2023-10-25 16:14:54 +0200

    Aufgeräumt

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-
+from datetime import datetime
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
+
+_now = datetime.utcnow()
+
 app = Flask(__name__,
             static_url_path='', 
             static_folder='web/static',
-            template_folder='web/templates')
+            template_folder='web/templates'
+            )
  
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
@@ -16,7 +20,7 @@ app = Flask(__name__,
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return render_template('index.html')
+    return render_template('content.html', now = _now)
  
 # main driver function
 if __name__ == '__main__':
@@ -24,3 +28,7 @@ if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
     app.run(debug=True)
+    
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-#from baseapp import app
-#import os
-
 #app.secret_key = os.urandom(24)
 #port = int(os.environ.get('PORT', 5000))
 #app.run(host='0.0.0.0', port=port)
